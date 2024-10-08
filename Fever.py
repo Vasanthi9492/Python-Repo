94e64b361097994df8d732ec4e81d6f174318c72
A.VASANTHI <159714662+Vasanthi9492@users.noreply.github.com>
2024-10-08 20:49:14 +0530

    Uploading code: Fever.py

---
+++ Fever.py
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Fever
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>98)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
